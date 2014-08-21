f85057f0ec1b13da35b3292ed81e5a38638d853c
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-08-21 11:17:49 +0200

    This will be implemented as a PageDecorator

---
+++ src/main/java/org/jenkinsci/plugins/pagenote/PageDecoratorImpl.java
@@ -0,0 +1,13 @@
+package org.jenkinsci.plugins.pagenote;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+
+/**
+ *
+ *
+ * @author Kohsuke Kawaguchi
+ */
+@Extension
+public class PageDecoratorImpl extends PageDecorator {
+}
