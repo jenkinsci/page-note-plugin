a67991a998df7592c3df6c6681f86021be2695d2
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-08-21 11:37:56 +0200

    Convert the current URL to a stable key

--- src/main/java/org/jenkinsci/plugins/pagenote/PageDecoratorImpl.java
+++ src/main/java/org/jenkinsci/plugins/pagenote/PageDecoratorImpl.java
@@ -1,7 +1,14 @@
 package org.jenkinsci.plugins.pagenote;
 
 import hudson.Extension;
+import hudson.Util;
+import hudson.model.Item;
 import hudson.model.PageDecorator;
+import hudson.model.Run;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.Ancestor;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerRequest;
 
 /**
  *
@@ -10,4 +17,38 @@ import hudson.model.PageDecorator;
  */
 @Extension
 public class PageDecoratorImpl extends PageDecorator {
+
+    /**
+     * Given the current page requested, turn that into a handle that identify the page uniquely
+     * even when the page is shown under different view, different permalink, etc.
+     *
+     * <p>
+     * This involves some heuristics and is done on best-effort basis.
+     */
+    public String key() {
+        StaplerRequest req = Stapler.getCurrentRequest();
+
+        // Run can be uniquely identified via Job
+        Ancestor a = req.findAncestor(Run.class);
+        if (a!=null) {
+            Run r = (Run)a.getObject();
+            return toId(r.getParent().getFullName(), r.getNumber(), a.getRestOfUrl());
+        }
+
+        // Job can be uniquely identified via its full name
+        a = req.findAncestor(Item.class);
+        if (a!=null) {
+            Item i = (Item)a.getObject();
+            return toId(i.getFullName(), a.getRestOfUrl());
+        }
+
+        // we don't have anything to contextualize this
+        return toId(req.getAncestors().get(0).getRestOfUrl());
+    }
+
+    private String toId(Object... tokens) {
+        return Util.getDigestOf(StringUtils.join(tokens,'\0'));
+    }
+
+
 }
